df578ee8028f801bae6d0a7849662da2cddc1496
manish kumar <manikuma0405@gmail.com>
2019-12-19 21:55:46 +0900

    不要なフャイル作成されない用にした

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module DevExam
   class Application < Rails::Application
+  	  config.generators do |g|
+      # この二行の記述で自動生成しない設定を作成しています。
+      g.assets false
+      g.helper false
+    end
+　　end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
